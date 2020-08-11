ff906846e03807997fd23d394733c33c8a34154a
Shaun Verch <shaun@shaunverch.com>
2020-08-10 21:27:39 -0400

    Add support for setting the bucket name

--- index.ts
+++ index.ts
@@ -2,8 +2,11 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
+// Get bucket name from configuration
+const config = new pulumi.Config();
+
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const bucket = new aws.s3.Bucket(config.require("bucket-name"));
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
