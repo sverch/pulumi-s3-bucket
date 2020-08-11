48271210c621bca7740dffb6044aab9f10b15e39
Shaun Verch <shaun@shaunverch.com>
2020-08-10 21:18:12 -0400

    Allow setting the bucket name

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
+		c := config.New(ctx, "")
+
 		// Create an AWS resource (S3 Bucket)
-		bucket, err := s3.NewBucket(ctx, "my-bucket", nil)
+		bucket, err := s3.NewBucket(ctx, c.Require("bucket-name"), nil)
 		if err != nil {
 			return err
 		}
